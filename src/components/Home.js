add5ed8359f5d5a80738e4faae1e83e4140306ed
Mykib <m.brims93@gmail.com>
2021-02-05 03:02:41 +0000

    added navigation and home

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import { Paper } from '@material-ui/core';
+import './Home.css';
+export default function Home () {
+    return (
+        <div className="home-component">
+            <Paper elevation={3} className="paper">
+            Content Here!
+            </Paper>
+        </div>
+        )
+}
