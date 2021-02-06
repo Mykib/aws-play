95db7af6737febc6a3313cda36b2d244d9b82fe6
Michael Brimo <michael@Michaels-MacBook-Pro.local>
2021-02-06 14:30:16 +1100

    Hello World!

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ export default function Home () {
     return (
         <div className="home-component">
             <Paper elevation={3} className="paper">
-            Content Here!
+                Hello World!!
             </Paper>
         </div>
         )
